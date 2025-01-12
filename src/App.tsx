7124bb6e4dc6ab7014d22956f5a624b9677d3321
“Jose <lealxh@gmail.com>
2025-01-12 19:15:09 -0300

    Initial config vite, tailwind, vitest

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <section id="app" className="m-auto 2xl:w-[1300px] py-10 px-5">
+    <h1 className="text-3xl font-bold underline text-red-500">
+      Hello world!
+    </h1>
+    </section>
+  )
+}
